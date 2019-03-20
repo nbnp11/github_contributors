216d1d4fde73617793482d50978894948e19b794
nbnp <nbnp11@gmail.com>
2019-03-20 22:17:18 +0300

    Add url validator

--- app/services/url_parser.rb
+++ app/services/url_parser.rb
@@ -6,6 +6,18 @@ class UrlParser
   end
 
   def call
+    binding.pry
+    return unless valid_url?
     params = @url.remove('https://github.com/').split('/')
+  end
+
+  private
+
+  def valid_url?
+    binding.pry
+    @testing = @url.remove('https://').split('/')
+    return false unless @testing.count == 3 # проверяем, что у нас путь только до репозитория
+    return false unless @testing.first.match?(/github.com/) # проверяем что домен верный
+    true
   end
 end
