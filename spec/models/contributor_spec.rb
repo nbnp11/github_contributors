76d716a20d7ba447b5d83e61ff66a59bdf467ffe
nbnp <nbnp11@gmail.com>
2019-03-22 13:35:35 +0300

    add model validation tests

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Contributor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation' do
+    it 'wont validate without login' do
+      contributor = Contributor.new()
+      expect(contributor.valid?).to be false
+    end
+
+    it 'wont validate without repo' do
+      contributor = Contributor.new(login: 'test')
+      expect(contributor.valid?).to be false
+    end
+
+    it 'will be valid with repo and login' do
+      repo = Repo.new(id: 1, user_name: 'test', repo_name: 'test')
+      contributor = repo.contributors.new(login: 'test')
+      expect(contributor.valid?).to be true
+    end
+  end
 end
