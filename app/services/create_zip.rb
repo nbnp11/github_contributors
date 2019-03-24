28573f4f6c6c47d8af05fcb34c5b0a2196cdb403
nbnp <nbnp11@gmail.com>
2019-03-24 19:08:19 +0300

    delete messages, add docker

--- app/services/create_zip.rb
+++ app/services/create_zip.rb
@@ -1,22 +1,30 @@
 require 'zip'
+require 'open-uri'
 
 class CreateZip
-  attr_accessor :repo_id
+  attr_accessor :repo_id, :base_url
 
-  def initialize(repo_id)
+  def initialize(repo_id, base_url)
     @repo_id = repo_id
+    @base_url = base_url
   end
 
   def call
-    @contributors_url = 'https://ghcontribs.herokuapp.com/contributors'
+    @contributors_url = "#{base_url}/contributors"
     @repo = Repo.find(@repo_id)
-    @archive_file = "#{Rails.root}/tmp/archive_#{DateTime.now.strftime("%B_%d_%Y_%k.%m")}.zip"
+    #binding.pry
+    @archive_file = "#{Rails.root}/tmp/archive_#{DateTime.now.strftime("%B_%d_%Y_%k.%M")}.zip"
     Zip::OutputStream.open(@archive_file) do |zos|
       @repo.contributors.each do |contributor|
+        #binding.pry
         @filename = "#{contributor.id}.pdf"
         @url = "#{@contributors_url}/#{@filename}"
         zos.put_next_entry(@filename)
-        zos.print(URI.parse(@url).read)
+        #zos.print(URI.parse(@url).read)
+        get_pdf_thread = Thread.new { @file = open(@url).read }
+        get_pdf_thread.join
+        zos.print(@file)
+        puts 'file added'
       end
     end
     @archive_file
